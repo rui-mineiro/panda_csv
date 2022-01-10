b68b8840fd54b30bc5cde2bfc3498a2187ede534
rui-mineiro <rui.mineiro@gmail.com>
2022-01-10 19:25:43 +0000

    bar

---
+++ show_csv_bar.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 21:54:37 2022
+
+@author: minas
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data=pd.read_csv('data.csv')
+X = list(data.iloc[:, 0])
+Y = list(data.iloc[:, 1])
+plt.bar(X, Y, color='g')
+plt.show()
